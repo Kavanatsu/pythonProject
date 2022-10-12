0538310ad60d657284fc54dcd580152e4a25e6de
Kavanatsu <20062000@inbox.ru>
2022-10-12 14:06:04 +0700

    1

--- locallibrary/locallibrary/catalog/admin.py
+++ locallibrary/locallibrary/catalog/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Genre, Language, Book, BookInstance, Author
+
+admin.site.register(Genre)
+admin.site.register(Language)
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
+
+# Define the admin class
+
+
+class BooksInLine(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BooksInLine]
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+# Register the Admin classes for Book using the decorator
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'language')
+
+    inlines = [BooksInstanceInline]
+
+
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
